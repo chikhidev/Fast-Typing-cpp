0b11fc83b9e92fc1783cda93ca2d60d56e9acf72
Abderrahim Chikhi <abchikhi@c3r2p7.1337.ma>
2025-11-06 13:28:34 +0100

    improvement

--- main.cc
+++ main.cc
@@ -30,11 +30,9 @@ struct bools
 
 struct score_tracking
 {
-    double time_took;
     int lines;
     unsigned int score;
-    int last_time_correct;
-    double last_line_time_taken;
+    size_t total_words;
 };
 
 
@@ -49,42 +47,53 @@ string_len
 }
 
 
-void
-print_current_summary
-(struct bools *bools_, struct score_tracking *track, double& took_line_time, double& time_advencement)
+// void
+// print_current_summary
+// (struct bools *bools_, struct score_tracking *track, double& took_line_time, double& time_advencement)
+// {
+//     (void)bools_;
+//     system("clear");
+//     if (track->score == 0) return;
+
+//     double score_percentage = (double(track->score) / MAX_SCORE) * 100;
+
+//     size_t i = 0;
+//     for (; i < 100; i++)
+//     {
+//         if (i < score_percentage)
+//             std::cout << YELLOW << "-";
+//         else
+//             std::cout << RESET << " ";
+//     }
+//     std::cout << " " << score_percentage << "%" << RESET << std::endl;
+
+//     if (track->last_line_time_taken != 0 && took_line_time > track->last_line_time_taken)
+//     {
+//         time_advencement = double(took_line_time / track->last_line_time_taken) * 100;
+//         std::cout << RED << "Speed downgrade from last time: " << time_advencement << "%" << RESET << std::endl;
+//     }
+//     else if (track->last_time_correct > 0)
+//     {
+//         time_advencement = double(track->last_line_time_taken / took_line_time) * 100;
+//         std::cout << GREEN << "Speed upgrade from last time: " << time_advencement << "%" << RESET << std::endl;
+//     }
+
+
+// }
+
+
+
+bool
+miss_cased_character
+(const char c, const char og)
 {
-    (void)bools_;
-    system("clear");
-    if (track->score == 0) return;
-
-    double score_percentage = (double(track->score) / MAX_SCORE) * 100;
-
-    size_t i = 0;
-    for (; i < 100; i++)
-    {
-        if (i < score_percentage)
-            std::cout << YELLOW << "-";
-        else
-            std::cout << RESET << " ";
-    }
-    std::cout << " " << score_percentage << "%" << RESET << std::endl;
-
-    if (track->last_line_time_taken != 0 && took_line_time > track->last_line_time_taken)
-    {
-        time_advencement = double(took_line_time / track->last_line_time_taken) * 100;
-        std::cout << RED << "Speed downgrade from last time: " << time_advencement << "%" << RESET << std::endl;
-    }
-    else if (track->last_time_correct > 0)
-    {
-        time_advencement = double(track->last_line_time_taken / took_line_time) * 100;
-        std::cout << GREEN << "Speed upgrade from last time: " << time_advencement << "%" << RESET << std::endl;
-    }
-
-
+    return (c == (og - 32) || c == (og + 32));
 }
 
 
-void proccess_single_word
+
+size_t
+proccess_single_word
 (struct bools *bools_, struct score_tracking *track,
      const std::string& expected, std::string& user_input,
      std::stringstream& diff)
@@ -94,6 +103,36 @@ void proccess_single_word
     (void)expected;
     (void)user_input;
     (void)diff;
+
+    size_t correct = 0;
+    size_t expected_size = expected.size();
+    size_t user_input_size = user_input.size();
+
+    if (expected_size > 0 && expected.c_str()[expected_size - 1] == '\n')
+        expected_size--;
+    if (user_input_size > 0 && user_input.c_str()[expected_size - 1] == '\n')
+        user_input_size--;
+
+    size_t i = 0;
+    for (; i < user_input_size; i++)
+    {
+        if (user_input.c_str()[i] == expected.c_str()[i])
+        {
+            diff << GREEN << user_input.c_str()[i];
+            correct++;
+        }
+        else if (miss_cased_character(user_input.c_str()[i], expected.c_str()[i]))
+            diff << YELLOW << user_input.c_str()[i];
+        else
+            diff << RED << user_input.c_str()[i];
+    }
+
+    for (; i < expected_size; i++)
+        diff << RED << user_input.c_str()[i];
+
+    diff << " ";
+
+    return correct;
 }
 
 
@@ -104,8 +143,9 @@ take_user_input
     size_t line_len = string_len(line);
     std::stringstream diff;
 
-    std::cout << line;
-    if (line[line_len - 1] != '\n') std::cout << std::endl;
+    std::cout << YELLOW << line << RESET;
+    if (line[line_len - 1] != '\n')
+        std::cout << std::endl;
 
     std::string input;
     std::clock_t line_start_typing_time = std::clock();
@@ -136,14 +176,27 @@ take_user_input
     line_stream.str(input);
 
     int i = 0;
+    size_t correct_sum = 0;
     while (std::getline(line_stream, word, ' '))
     {
-        proccess_single_word(bools_, track, expected_words[i], word, diff);
+        correct_sum += proccess_single_word(bools_, track, expected_words[i], word, diff);
+        if (correct_sum == word.size())
+            track->total_words++;
+        i++;
     } 
 
+    double line_took_time = (double)(line_end_typing_time - line_start_typing_time) / CLOCKS_PER_SEC;
 
-    (void)track;
-    (void)line_start_typing_time;
+    system("clear");
+
+    diff << RESET;
+    std::cout << line << std::endl;
+    std::cout << diff.str() << std::endl;
+    std::cout << "Correct: " << correct_sum << std::endl;
+    std::cout << "Time took: " << line_took_time << std::endl;
+    std::cout << std::endl;
+    
+    track->lines++;
 }
 
 int
@@ -168,19 +221,14 @@ main
 
     system("clear");
 
-    std::clock_t start = std::clock();
-
     while (getline(&line, &line_len, input) > 0) {
         if (bools_.stop) break;
         take_user_input(&bools_, &track, line);
-
     }
 
-    track.time_took = double(std::clock() - start) / CLOCKS_PER_SEC;
-    std::cout << "Summary:" << std::endl;
-    std::cout << "N of lines: " << track.lines << std::endl;
-    std::cout << "Time took: " << track.time_took << " MS" << std::endl;
-    std::cout << "Score: " << (track.time_took > 0 ? track.score / track.time_took : 0) << std::endl;
+    std::cout << "Summary:-------------------" << std::endl;
+    std::cout << "N of lines:\t" << track.lines << std::endl;
+    std::cout << "Total words:\t" << track.total_words << std::endl;
 
     return success;
 }
